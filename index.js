a4cf94ec9e9a242dfe0a75527de7bf087c046ece
andydick1947 <ardick2000@hotmail.com>
2025-01-11 20:53:53 +0000

    notes

--- index.js
+++ index.js
@@ -55,16 +55,31 @@ function tgle(){
      offScreenMenu.classList.toggle("active");
    };
 
+   function myintro1(){
+      const int1='<p id="myintro1" class="mytxt">INTRODUCTION</p>';
+      const div1=document.createElement("div");
+      const mydiv1=document.getElementById("mainsect");
+      mydiv1.appendChild(div1);
+      div1.innerHTML=int1;
+}
+
 function myintro(){
-      const int='<p id="myintro" class="mytxt">INTRODUCTION<br><br>The Bridge Club was founded around 1996 as the Monday Night Bridge Club, the first Director being Derek Branch. The Club continues to meet in the Llantonian Hall.  The aim of the club is to provide a regular facility for Duplicate Bridge allowing all pairs to play the same boards over the course of the evening. Ideally people attend in pairs although every attempt is made to cater for those without a regular partner.</p>';
+      const int='<p id="myintro" class="mytxt"><br><br>The Bridge Club was founded around 1996 as the Monday Night Bridge Club, the first Director being Derek Branch. The Club continues to meet in the Llantonian Hall.  The aim of the club is to provide a regular facility for Duplicate Bridge allowing all pairs to play the same boards over the course of the evening. Ideally people attend in pairs although every attempt is made to cater for those without a regular partner.</p>';
       const div=document.createElement("div");
       const mydiv=document.getElementById("mainsect");
       mydiv.appendChild(div);
       div.innerHTML=int;
 }
 
+function myloc1(){
+   const loc1='<p class="mytxt">LOCATION<br></p>';
+   const div1=document.createElement("div");
+   const mydiv1=document.getElementById("mainsect");
+   mydiv1.appendChild(div1);
+   div1.innerHTML=loc1;
+}
 function myloc(){
-   const loc='<p id="myloc" class="mytxt">LOCATION<br><br>The Llantonian Hall is situated at the far end of the Car Park it shares with the Rugby Club. Access is directly off the roundabout passed the surgery.</p>';
+   const loc='<p class="mytxt"><br>The Llantonian Hall is situated at the far end of the Car Park it shares with the Rugby Club. Access is directly off the roundabout passed the surgery.</p>';
    const div=document.createElement("div");
    const mydiv=document.getElementById("mainsect");
    mydiv.appendChild(div);
@@ -76,9 +91,16 @@ function myloc(){
    mydiv1.appendChild(div1);
    div1.innerHTML=loc1;
 }
+function mycttee1(){
+const cte1='<p class="mytxt">THE COMMITTEE</p>'
+const div1=document.createElement("div");
+const mydiv1=document.getElementById("mainsect");
+mydiv1.appendChild(div1);
+div1.innerHTML=cte1;
+}
 
 function mycttee(){
-      const cte='<table id="tbl"><tr><th colspan="2">THE COMMITTEE</th></tr><tr><td>Chairman/Director:</td><td>Allan Evans</td></tr><tr><td>Treasurer:</td><td>Andrew Leonard</td><tr><td>Seceratary/Partnership Coordinator:</td><td>Meryl Phillips</td></tr><tr><td>Scorer:</td><td>Andy Dick</td></tr></table><br>';
+      const cte='<table id="tbl"><tr><td>Chairman/Director:</td><td>Allan Evans</td></tr><tr><td>Treasurer:</td><td>Andrew Leonard</td><tr><td>Seceratary/Partnership Coordinator:</td><td>Meryl Phillips</td></tr><tr><td>Scorer:</td><td>Andy Dick</td></tr></table><br>';
       const div=document.createElement("div");
       const mydiv=document.getElementById("mainsect");
       mydiv.appendChild(div);
@@ -91,21 +113,37 @@ function mycttee(){
    div1.innerHTML=cte1;
 }
 
+function mymeets1(){
+   const mts1='<p class="mytxt">MEETINGS<br></p>';
+      const div1=document.createElement("div");
+      const mydiv1=document.getElementById("mainsect");
+      mydiv1.appendChild(div1);
+      div1.innerHTML=mts1;
+}
+
 function mymeets(){
-   const mts='<p id="meets" class="mytxt">MEETINGS<br><br>The club meets on Monday evenings at 6.45, with play starting at 7.00 and aiming to finish by 10.00. There are also two party evenings each year</p>';
+   const mts='<p id="meets" class="mytxt">The club meets on Monday evenings at 6.45, with play starting at 7.00 and aiming to finish by 10.00. There are also two party evenings each year</p>';
       const div=document.createElement("div");
       const mydiv=document.getElementById("mainsect");
       mydiv.appendChild(div);
       div.innerHTML=mts;
 }
 
+function myfees1(){
+   const fees1='<p class="mytxt">FEES<br></p>';
+      const div1=document.createElement("div");
+      const mydiv1=document.getElementById("mainsect");
+      mydiv1.appendChild(div1);
+      div1.innerHTML=fees1;
+}
 function myfees(){
-   const fees='<p id="mycttee" class="mytxt">FEES<br><br>The Fees are paid annually in January. They have been held steady at £65 for the year 2025. There is no Table charge for members, however, visitors pay £4 for each session.</p>';
+   const fees='<p class="mytxt">The Fees are paid annually in January. They have been held steady at £65 for the year 2025. There is no Table charge for members, however, visitors pay £4 for each session.</p>';
       const div=document.createElement("div");
       const mydiv=document.getElementById("mainsect");
       mydiv.appendChild(div);
       div.innerHTML=fees;
 }
+
 
 function myres(){
       const res='<iframe id="frm1" src="res-mnths.html"></iframe>';
@@ -138,12 +176,14 @@ for (let x=1; x<=7; x++){
  
     if(z=="debtn1"){ 
        clean();
+       myintro1();
        myintro();
        tgle()
     }
  
     if(z=="debtn2"){ 
        clean()
+       myloc1()
        myloc()
        tgle()
    
@@ -151,18 +191,21 @@ for (let x=1; x<=7; x++){
  
     if(z=="debtn3"){ 
        clean()
+       mycttee1()
        mycttee()
        tgle()
     }
  
     if(z=="debtn4"){ 
        clean()
+       mymeets1()
        mymeets()
        tgle()
     }
  
     if(z=="debtn5"){ 
        clean()
+       myfees1()
        myfees()
        tgle()
     }
